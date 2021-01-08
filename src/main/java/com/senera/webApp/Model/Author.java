7ec48c07e9ba7457b0932051533be5466e6f9dac
senera <mallikarachchi96@gmail.com>
2021-01-08 16:37:34 +0530

    mapped two entities.

--- src/main/java/com/senera/webApp/Model/Author.java
+++ src/main/java/com/senera/webApp/Model/Author.java
@@ -1,19 +1,65 @@
 package com.senera.webApp.Model;
 
+import javax.persistence.*;
 import java.util.Set;
 
 /**
  * @author Senera Madushan
  * @comment
  */
-
+@Entity
 public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
 
     private String firstName;
     private String lastName;
+
+    @ManyToMany(mappedBy = "authhors")
     private Set<Book> books;
 
 
+    public Author() {
+    }
+
+    public Author(String firstName, String lastName, Set<Book> books) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.books = books;
+    }
 
 
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Set<Book> getBooks() {
+        return books;
+    }
+
+    public void setBooks(Set<Book> books) {
+        this.books = books;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
--- src/main/java/com/senera/webApp/Model/Book.java
+++ src/main/java/com/senera/webApp/Model/Book.java
@@ -1,8 +1,65 @@
 package com.senera.webApp.Model;
 
+import javax.persistence.*;
+import java.util.Set;
+
 /**
  * @author Senera Madushan
  * @comment
  */
+@Entity
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String isbn;
+    private String title;
+
+    @ManyToMany
+    @JoinTable(name = "author_book",joinColumns = @JoinColumn(name = "book_id"),
+            inverseJoinColumns = @JoinColumn(name = "author_id"))
+    private Set<Author> authors;
+
+    public Book() {
+    }
+
+    public Book(String isbn, String title, Set<Author> authors) {
+        this.isbn = isbn;
+        this.title = title;
+        this.authors = authors;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Set<Author> getAuthors() {
+        return authors;
+    }
+
+    public void setAuthors(Set<Author> authors) {
+        this.authors = authors;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
 }
