68e5fbd666453adc108fb4c4da47763c073425b0
senera <mallikarachchi96@gmail.com>
2021-01-08 20:15:42 +0530

    overide equals and hash methods.

--- src/main/java/com/senera/webApp/Model/Book.java
+++ src/main/java/com/senera/webApp/Model/Book.java
@@ -62,4 +62,30 @@ public class Book {
     public void setId(Long id) {
         this.id = id;
     }
+
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", isbn='" + isbn + '\'' +
+                ", title='" + title + '\'' +
+                ", authors=" + authors +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Book book = (Book) o;
+
+        return id != null ? id.equals(book.id) : book.id == null;
+    }
+
+    @Override
+    public int hashCode() {
+        return id != null ? id.hashCode() : 0;
+    }
 }
